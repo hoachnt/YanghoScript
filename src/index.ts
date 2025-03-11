d24c53eebc0a4997424c7ee16979d964e7ddbefc
hoachnt <hoachprogrammer@gmail.com>
2025-03-11 17:12:08 +0700

    feat: enable running arbitrary YanghoScript files via CLI

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-import interpretCode from "./interpretator";
+#!/usr/bin/env node
+
+import interpretCode from "./cli";
 import fs from "node:fs";
+import path from "node:path";
+
+if (!process.argv[2]) {
+	console.error("Error: You must specify a path to a file.");
+	process.exit(1);
+}
+
+const filePath = process.argv[2];
+const absolutePath = path.isAbsolute(filePath)
+	? filePath
+	: path.join(process.cwd(), filePath);
 
 try {
-  const data = fs.readFileSync("./src/code.ys", "utf8");
-
-  interpretCode(data);
+	const data = fs.readFileSync(absolutePath, "utf8");
+	interpretCode(data);
 } catch (err) {
-  console.error(err);
+	console.error(err);
 }
