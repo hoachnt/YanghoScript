c3308dc34bbc8c637ea88aa468ad0f2648f15fcd
hoachnt <hoachprogrammer@gmail.com>
2025-03-13 20:28:05 +0700

    refactor: adapt Interpreter, Lexer, and Parser to new tokens structure

--- src/Interpreter.ts
+++ src/Interpreter.ts
@@ -9,7 +9,7 @@ import StatementsNode from "./AST/StatementsNode";
 import StringNode from "./AST/StringNode";
 import UnarOperationNode from "./AST/UnarOperationNode";
 import VariableNode from "./AST/VariableNode";
-import { TokenNames, getTokenType } from "./TokenType";
+import { useTokenType } from "./tokens";
 
 // Define an immutable environment interface.
 export interface Environment {
@@ -28,6 +28,8 @@ class ReturnException {
 }
 
 function evaluate(node: ExpressionNode, env: Environment): EvalResult {
+	const { getTokenType, TokenNames } = useTokenType();
+
 	if (node instanceof ReturnNode) {
 		const result = evaluate(node.expression, env);
 		throw new ReturnException(result.value);
